37eb1b70db71aafbf86f25e2db9350dbacc03296
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-09-09 12:30:00 +0300

    add  *

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { TextButton } from './Buttons/TextButton/TextButton.jsx';
 export { IconButton } from './Buttons/IconButton/IconButton.jsx';
-export { SearchBar } from './SearchBar/SearchBar.jsx';
+export * from './SearchBar/SearchBar.jsx';
 export { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem.jsx';
 export { ImageGallery } from './ImageGallery/ImageGallery.jsx';
 export { Loader } from './Loader/Loader.jsx';
